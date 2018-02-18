771585d374f7c9d3e9f8502a7e8e6a9d1db21b7c
Gibson <gibsonfischer1@gmail.com>
2018-02-18 16:22:47 -0600

    added new ish

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let aNumber = 69
+let aString = 'test for GIS class'
+let aBoolean = true
+let aArray = ['sunday', 'monday', 'tuesday']
+console.log(aNumber)
+console.log(aString)
+console.log(aBoolean)
+console.log(aArray)
